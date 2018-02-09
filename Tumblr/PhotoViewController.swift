2812cf2d38702b785ac2b44b47eeb1b811fcad24
Tavien <tavien@webccvm.morgan.edu>
2018-02-09 13:01:59 -0800

    fixed app and added new gif

--- Tumblr/PhotoViewController.swift
+++ Tumblr/PhotoViewController.swift
@@ -11,7 +11,7 @@ import AlamofireImage
 
 class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
-   
+    
     @IBOutlet weak var tableView: UITableView!
      var posts: [[String: Any]] = []
     
@@ -21,7 +21,7 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
         tableView.dataSource = self
         tableView.delegate = self
         
-        
+       //Network Request
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
@@ -30,25 +30,27 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
                 print(error.localizedDescription)
             } else if let data = data,
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                print(dataDictionary)
-                
-                // Get the dictionary from the response key
+                //print(dataDictionary)
+
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
-                // Store the returned array of dictionaries in our posts property
+                
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
                 
-                // TODO: Get the posts and store in posts property
+                self.tableView.reloadData()
                 
+                // TODO: Get the posts and store in posts property
                 // TODO: Reload the table view
             }
         }
         task.resume()        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
        return posts.count
@@ -69,14 +71,15 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
             let urlString = originalSize["url"] as! String
             // 4.
             let url = URL(string: urlString)
-           
+            
             cell.photoViewImage.af_setImage(withURL: url!)
+           // self.tableView.reloadData()
         }
         
-        
-        self.tableView.reloadData()
         return cell
     }
+    
+   
     
     /*
     // MARK: - Navigation
